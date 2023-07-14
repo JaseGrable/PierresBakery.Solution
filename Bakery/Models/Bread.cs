f151c53869feaf5bcaa33bbfa5abe6658e436afd
JaseGrable <jase.grable@gmail.com>
2023-07-14 16:38:43 -0700

    Add and pass test to count bread

---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Bakery.Models
+
+{
+    public class Bread
+    {
+        public int BreadCount { get; }
+        public Bread(int count)
+        {
+            BreadCount = count;
+        }
+    }
+}
