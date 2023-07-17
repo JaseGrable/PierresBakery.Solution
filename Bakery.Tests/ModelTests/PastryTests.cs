af2274d8a7cdd9cfeed3b7075f4c81c6110b9503
JaseGrable <jase.grable@gmail.com>
2023-07-16 21:24:21 -0700

    Add and pass test for creation of pastry constructor

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -2,3 +2,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
 using System.Collections.Generic;
 using System;
+
+
+namespace Bakery.Tests
+{
+    [TestClass]
+    public class PastryTests
+    {
+        [TestMethod]
+        public void PastryConstructor_CreatesInstancesOfPastryClass_Pastry()
+        {
+            Pastry newPastry = new Pastry();
+            Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+        }
+    }
+}
---
+++ Bakery/Models/Pastry.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Models
+{
+    public class Pastry
+    {
+        public Pastry()
+        {
+
+        }
+    }
+}
