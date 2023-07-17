c3611e4f086dcd52579799b61675162161a31095
JaseGrable <jase.grable@gmail.com>
2023-07-17 08:14:20 -0700

    Update UI to calculate Bread Price

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -15,6 +15,25 @@ namespace Bakery
             Console.WriteLine("Bread loaves cost $5 each");
             Console.WriteLine("Pastries are $2 each");
             Console.WriteLine("Remember! Buy 2 loaves of bread, get 1 free & Buy 3 pastries get 1 free");
+            int discountBreadPrice = Bread();
+        }
+        static int Bread()
+        {
+            Console.WriteLine("Bread Loaves $5 each, please enter how many loaves you would like");
+            string input = Console.ReadLine();
+            if (Int32.TryParse(input, out int BreadCount))
+            {
+                Bread breadOrder = new Bread(BreadCount);
+                int breadPrice = breadOrder.BreadCost();
+                int discountBreadPrice = breadOrder.BreadDiscount(breadPrice);
+                Console.WriteLine("$" + discountBreadPrice);
+                return discountBreadPrice;
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number");
+                return Bread();
+            }
         }
     }
 }
